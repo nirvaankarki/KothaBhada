e5e3a2f1e42f5682c01b4568ac61629e2964d819
nirvaankarki <nirojkarki.personal@email.com>
2026-04-24 13:42:19 +0545

    fix: case sensitivity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import { App } from './App.jsx'
+import { App } from './app.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
